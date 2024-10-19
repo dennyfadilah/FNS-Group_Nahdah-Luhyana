b5d0007fac96fb5d8434a0525798688a16feee16
dennyfadilah <dennyfadilah99@gmail.com>
2024-10-19 14:57:47 +0700

    add jquery, datatables,

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -6,11 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= base_url('vendor/bootstrap/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('vendor/bootstrap-icons/font/bootstrap-icons.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('vendor/datatables/css/datatables.min.css') ?>">
 </head>
 
 <body>
+
     <?= $this->renderSection('content') ?>
-    <script src="<?= base_url('vendor/bootrap/js/bootstrap.bundle.js') ?>"></script>
+
+    <script src="<?= base_url('vendor/bootrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= base_url('vendor/jquery/jquery.js') ?>"></script>
+    <script src="<?= base_url('vendor/datatables/js/datatables.min.js') ?>"></script>
 </body>
 
 </html>
