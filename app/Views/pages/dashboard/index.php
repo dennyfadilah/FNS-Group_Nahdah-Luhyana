7b6efbb44f5048297bee6561f6c90ceb61e50eba
dennyfadilah <dennyfadilah99@gmail.com>
2025-03-08 23:16:19 +0700

    Create Project FNS Group

---
+++ app/Views/pages/dashboard/index.php
@@ -0,0 +1,10 @@
+<?= $this->extend('layouts/dashboard') ?>
+
+<?= $this->section('content') ?>
+<title>Dashboard</title>
+
+<div class="card p-2 bg-warning text-center">
+    <i class="bi bi-exclamation-triangle fs-1"></i>
+    <p class="lead my-0">Views/Dashboard</p>
+</div>
+<?= $this->endSection() ?>
